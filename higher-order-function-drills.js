789e5382ec6baa6b26e4f0c8d730a5e199d41839
Stephen Anderson <anderssj@usc.edu>
2018-10-30 09:22:54 -0700

    filter function

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -1,16 +1,41 @@
 const repeat = (fn, n) => {
   for (let i = 0; i < n; i++) {
-    fn();
+    fn()
   }
 }
 
 const hello = () => {
-  console.log('Hello world');
+  console.log('Hello world')
 }
 
 const goodbye = () => {
-  console.log('Goodbye world');
+  console.log('Goodbye world')
 }
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+repeat(hello, 5)
+repeat(goodbye, 5)
+
+function filter(arr, fn) {
+  let newArray = []
+  arr.forEach(element => {
+    if (fn(element)) {
+      newArray.push(element)
+    }
+  })
+  return newArray
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray']
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that
+  // only returns a boolean
+  return name[0] === 'R'
+})
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
